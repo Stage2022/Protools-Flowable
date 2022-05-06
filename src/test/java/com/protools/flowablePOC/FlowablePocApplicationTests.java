e2031dce211629d4c1ac24ef74acdda2e8197cd1
MailineN <nguyen.mailine@gmail.com>
2022-05-06 16:40:02 +0200

    first commit

---
+++ src/test/java/com/protools/flowablePOC/FlowablePocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.protools.flowablePOC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowablePocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
