6d86d78921ef595ebd19f68fec2fa7d00525c426
Mailine Nguyen <nguyen.mailine@gmail.com>
2022-06-16 10:23:51 +0200

    test event deployments

---
+++ src/main/java/com/protools/flowablePOC/services/EventInfoService.java
@@ -0,0 +1,23 @@
+package com.protools.flowablePOC.services;
+
+import org.flowable.engine.history.HistoricProcessInstance;
+import org.flowable.eventregistry.api.EventDeployment;
+import org.flowable.eventregistry.api.EventRepositoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class EventInfoService {
+    @Autowired
+    private EventRepositoryService eventRepositoryService;
+
+    @Transactional
+    public List<EventDeployment> getEventDeployments(){
+        List<EventDeployment> response = eventRepositoryService.createDeploymentQuery().list();
+
+        return (response);
+    };
+}
